e756efa19f1a58543067cf3a8783419266433abc
noname <noname@MacBook-Air.local>
2022-12-16 23:32:17 +0300

    add model

--- one/one/urls.py
+++ one/one/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from one import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from human.views import *
 
@@ -21,4 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('human.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = pageNotFound
