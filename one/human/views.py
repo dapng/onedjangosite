957ac9a5357e6d3c4b2ed12b47054973acd0a55d
noname <noname@MacBook-Air.local>
2023-01-04 18:04:51 +0300

    feat: add create, show, category

--- one/human/views.py
+++ one/human/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import get_object_or_404, render, redirect
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .forms import AddPostForm
 from .models import Human, Category
 
@@ -24,6 +25,9 @@ class HumanHome(ListView):
         context['title'] = 'Главная'
         context['cat_selected'] = 0
         return context
+    
+    def get_queryset(self):
+        return Human.objects.filter(is_published=True)
 
 
 # def index(request):
@@ -43,18 +47,30 @@ def about(request):
         {'menu': menu, 'title': 'About page'})
 
 
-def add_page(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            try:
-                form.save()
-                return redirect('home')
-            except Exception:
-                form.add_error(None, 'Ошибка добавления')
-    else:
-        form = AddPostForm()
-    return render(request, 'human/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'human/addpage.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Добавление статьи'
+        return context
+
+
+# def add_page(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             try:
+#                 form.save()
+#                 return redirect('home')
+#             except Exception:
+#                 form.add_error(None, 'Ошибка добавления')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'human/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(reauest):
@@ -65,29 +81,59 @@ def login(request):
     return HttpResponse('Login page')
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Human, slug=post_slug)
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id}
-    return render(request, 'human/post.html', context=context)
+class ShowPost(DetailView):
+    model = Human
+    template_name = 'human/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
 
 
-def show_category(request, cat_slug):
-    cat = Category.objects.filter(slug=cat_slug)
-    posts = Human.objects.filter(cat_id=cat[0].id)
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Human, slug=post_slug)
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post.cat_id}
+#     return render(request, 'human/post.html', context=context)
 
-    if len(posts) == 0:
-        raise Http404
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Отображение по категориям',
-        'cat_selected': cat[0].id}
-    return render(request, 'human/index.html', context=context)
+class HumanCategory(ListView):
+    model = Human
+    template_name = 'human/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Human.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория -' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+
+# def show_category(request, cat_slug):
+#     cat = Category.objects.filter(slug=cat_slug)
+#     posts = Human.objects.filter(cat_id=cat[0].id)
+
+#     if len(posts) == 0:
+#         raise Http404
+
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Отображение по категориям',
+#         'cat_selected': cat[0].id}
+#     return render(request, 'human/index.html', context=context)
 
 
 def pageNotFound(request, exception):
