9d4176e8aafe8b3ad8ae2f3b3e13fb1334cb88fa
dapng <da.png@yandex.ru>
2023-01-05 14:45:49 +0300

    feat: del not uset code

--- one/human/views.py
+++ one/human/views.py
@@ -30,16 +30,6 @@ class HumanHome(ListView):
         return Human.objects.filter(is_published=True)
 
 
-# def index(request):
-#     posts = Human.objects.all()
-#     context = {
-#         'posts': posts,
-#         'menu': menu,
-#         'title': 'Main page',
-#         'cat_selected': 0}
-#     return render(request, 'human/index.html', context=context)
-
-
 def about(request):
     return render(
         request,
@@ -57,20 +47,6 @@ class AddPage(CreateView):
         context['menu'] = menu
         context['title'] = 'Добавление статьи'
         return context
-
-
-# def add_page(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             try:
-#                 form.save()
-#                 return redirect('home')
-#             except Exception:
-#                 form.add_error(None, 'Ошибка добавления')
-#     else:
-#         form = AddPostForm()
-#     return render(request, 'human/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(reauest):
@@ -94,16 +70,6 @@ class ShowPost(DetailView):
         return context
 
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Human, slug=post_slug)
-#     context = {
-#         'post': post,
-#         'menu': menu,
-#         'title': post.title,
-#         'cat_selected': post.cat_id}
-#     return render(request, 'human/post.html', context=context)
-
-
 class HumanCategory(ListView):
     model = Human
     template_name = 'human/index.html'
@@ -119,21 +85,6 @@ class HumanCategory(ListView):
         context['menu'] = menu
         context['cat_selected'] = context['posts'][0].cat_id
         return context
-
-
-# def show_category(request, cat_slug):
-#     cat = Category.objects.filter(slug=cat_slug)
-#     posts = Human.objects.filter(cat_id=cat[0].id)
-
-#     if len(posts) == 0:
-#         raise Http404
-
-#     context = {
-#         'posts': posts,
-#         'menu': menu,
-#         'title': 'Отображение по категориям',
-#         'cat_selected': cat[0].id}
-#     return render(request, 'human/index.html', context=context)
 
 
 def pageNotFound(request, exception):
