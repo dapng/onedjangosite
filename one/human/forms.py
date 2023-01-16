37be761a643c87abf856d4c7b80f7d0fade3c66f
Данила Богославцев <da.png@yandex.ru>
2023-01-16 17:35:53 +0300

    feat: add auth and register

--- one/human/forms.py
+++ one/human/forms.py
@@ -32,6 +32,7 @@ class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
