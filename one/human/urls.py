86778c1c1e0674b180c90d3b4401851747a12970
Данила Богославцев <da.png@yandex.ru>
2023-01-16 17:33:31 +0300

    feat: add register and auth

--- one/human/urls.py
+++ one/human/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import about, contact, login, AddPage, HumanHome, HumanCategory, ShowPost
+from .views import about, contact, AddPage, HumanHome, HumanCategory, RegisterUser, ShowPost, LoginUser, logout_user
 
 urlpatterns = [
     path('', HumanHome.as_view(), name='home'),
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
-    path('login/', login, name='login'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', HumanCategory.as_view(), name='category')
 ]
